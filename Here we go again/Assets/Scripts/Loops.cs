ff821a60cdaf9618b2ea3b44a52fbe90e559a755
mmitchell25-dos <244543515+Jmitchell25-HYP@users.noreply.github.com>
2026-06-05 12:45:28 +0100

    Learning

---
+++ Here we go again/Assets/Scripts/Loops.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Globalization;
+
+public class Loops : MonoBehaviour
+{
+   
+    public bool isLooped;
+    public bool hasDelhi;
+    public bool hasDubai;
+    public int intelligence = 67;
+
+    public enum spacial
+    {
+        hase5haa534h,
+        tgy98wegfQP9I,
+        AWESYFUWIFEAgiu,
+        efiuefgueiwf,
+        efgvvuvw,
+        vfqevfivbsduvg,
+        ehggerv4hrvhergoehgrihoefrb,
+        rvrwubvuiwbvibwerivbwvbwibve,
+        r4yvv5ybvy5y43gyevhwve,
+        bbewrcgecv
+
+    }
+
+    
+    void Start()
+    {
+        List<string> loops = new List<string>();
+        List<double> marathons = new List<double>();
+        List<string> citiesList = new List<string> { "Delhi", "Los Angeles" };
+        int numberCitiesList = citiesList.Count;
+        Console.WriteLine(marathons.Count);
+        marathons.Add(143.23);
+        Console.WriteLine(marathons.Contains(143.23));
+        Console.WriteLine(marathons.Count);
+        Console.WriteLine(marathons.Count());
+        bool success = citiesList.Remove("Delhi)");
+        Console.WriteLine(marathons[1]);
+        bool removed = marathons.Remove(143.12);
+        Console.WriteLine(marathons[1]);
+        Console.WriteLine(removed);
+
+
+        
+
+
+
+        bool hasDelhi = citiesList.Contains("Delhi");
+        bool hasDubai = citiesList.Contains("Dubai");
+        {
+            Console.WriteLine("Speech imbedded");
+
+
+        }
+        if (isLooped != false)
+        {
+            Console.WriteLine("It is inactive");
+        }
+    }
+
+
+
+    void Update()
+    {
+        
+    }
+
+    void Calculator()
+    {
+        
+
+    }
+
+ 
+}
