8838474ff27c1e368f7367c15a4169a8d340b11c
Jmitchell25-HYP <jmitchell25@elam.co.uk>
2026-06-23 12:45:09 +0100

    Making progress

--- Here we go again/Assets/Scenes/Scripts/EnemyAI.cs
+++ Here we go again/Assets/Scenes/Scripts/EnemyAI.cs
@@ -3,28 +3,125 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    public NavMeshAgent agent;
 
-    public Transform Player;
+    enum AIState
+    {
+        Idle, Patrolling, chasing
+    }
 
-    public LayerMask WhatIsGround, whatIsPlayer;
+    [Header("Patrol")]
+    [SerializeField] private Transform Waypoints;
+    [SerializeField] private float waitAtPoint;
+    private int currentWaypoint;
+    private float waitCounter;
 
-    public float health;
+    [Header("Components")]
+    NavMeshAgent agent;
 
-    //Patroling
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPontRange;
+    [Header("AI states")]
+    [SerializeField] private AIState currentState;
 
-    //Attacking
-    public float timeBetweenAttacks;
-    bool alreadyAttacked;
+    [Header("Chasing")]
+    [SerializeField] private float chaseRange;
 
-    //States
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
+    [Header("Suspicious")]
+    [SerializeField] private float suspiciousTime;
+    private float timeSinceLastSawPlayer;
 
-    private v
+    private GameObject player;
+
+
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        waitCounter = waitAtPoint;
+        timeSinceLastSawPlayer = suspiciousTime;
+            
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        switch (currentState)
+        {
+            case AIState.Idle:
+
+                if (waitCounter > 0)
+                {
+                    waitCounter -= Time.deltaTime;
+                }
+                else
+                {
+                    currentState = AIState.Patrolling;
+                    agent.SetDestination(Waypoints.GetChild(currentWaypoint).position);
+                }
+
+                if(distanceToPlayer <= chaseRange)
+                {
+                    currentState = AIState.chasing;
+                }
+
+                break;
+
+
+            case AIState.Patrolling:
+
+                if (agent.remainingDistance <= 0.2f)
+                {
+                    currentWaypoint++;
+                    if (currentWaypoint >= Waypoints.childCount)
+                    {
+                        currentWaypoint = 0;
+                    }
+                    currentState = AIState.Idle;
+                    waitCounter = waitAtPoint;
+                }
+
+                if (distanceToPlayer <= chaseRange)
+                {
+
+                    currentState = AIState.chasing;
+                }
+
+                break;
+
+
+            case AIState.chasing:
+                agent.SetDestination(player.transform.position);
+                if (distanceToPlayer > chaseRange)
+                {
+                    agent.isStopped = true;
+                    agent.velocity = Vector3.zero;
+                    timeSinceLastSawPlayer -= Time.deltaTime;
+
+                    if (timeSinceLastSawPlayer < 0)
+                    {
+                        currentState = AIState.Idle;
+                        timeSinceLastSawPlayer = suspiciousTime;
+                        agent.isStopped = false;
+
+                    }
+                }
+
+                break;
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+
 
 
 
