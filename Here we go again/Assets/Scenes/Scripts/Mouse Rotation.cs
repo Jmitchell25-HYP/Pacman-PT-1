7e899f3aa3ee772714ed633519c70e8d64dc3147
Jmitchell25-HYP <jmitchell25@elam.co.uk>
2026-07-07 17:01:10 +0100

    Still needs fixing

--- Here we go again/Assets/Scenes/Scripts/Mouse Rotation.cs
+++ Here we go again/Assets/Scenes/Scripts/Mouse Rotation.cs
@@ -4,11 +4,15 @@ public class MouseRotation : MonoBehaviour
 {
     [SerializeField] private Transform playerBody;
     [SerializeField] private float Sensitivity = 0.1f;
+    private InputAction playerControls;
 
     private Vector2 lookInput;
     private float xRotation = 0f;
     public float moveSpeed = 5f;
     private Vector2 moveInput;
+    Vector2 moveDirection = Vector2.zero;
+    private Rigidbody rb;
+
 
     public void OnMove(InputValue value)
     {
@@ -19,6 +23,7 @@ public class MouseRotation : MonoBehaviour
     {
         Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y);
         transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+        rb.linearVelocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 
     public void OnLook(InputValue value)
@@ -31,41 +36,35 @@ public class MouseRotation : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
-    
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
     void Update()
     {
         float mouseX = lookInput.x * Sensitivity;
         float mouseY = lookInput.y * Sensitivity;
 
         // Camera up / down
-        xRotation -= mouseY;
+        xRotation -= mouseX;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.localRotation = Quaternion.Euler( 0f, xRotation, 0f);
+
+        moveDirection = playerControls.ReadValue<Vector2>();
 
         // Player  left / right
-        playerBody.Rotate(Vector3.up * mouseX);
-
-
-
-
-
-
-
-
+        //playerBody.Rotate(Vector3.up * mouseX);
 
 
     }
-
-
-
-
-
-
-
-
-
-
 
 
 
